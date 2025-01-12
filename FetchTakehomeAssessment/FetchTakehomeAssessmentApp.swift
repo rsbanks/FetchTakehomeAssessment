fe45dce86a986a96b04a26c3a96a869a35b5a5a4
rebanks <rebanks@expediagroup.com>
2025-01-11 19:56:54 -0800

    Initial Commit

---
+++ FetchTakehomeAssessment/FetchTakehomeAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakehomeAssessmentApp.swift
+//  FetchTakehomeAssessment
+//
+//  Created by Rebecca Banks on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakehomeAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
